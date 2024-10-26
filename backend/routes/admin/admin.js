980d73d77e40e3a846745e1fe3a62d6d05cda116
KOUSHIK PINNU <pinnukoushik1@gmail.com>
2024-10-26 18:55:31 +0530

    first

--- backend/routes/admin/admin.js
+++ backend/routes/admin/admin.js
@@ -2,4 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
+router.post('/login', function(req, res){
+    
+})
+
+
 module.exports =router
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route , Routes , BrowserRouter} from 'react-router-dom'
+import AdminLogin from './components/admins/AdminLogin';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+       <Routes>
+        <Route path='/admin/login' Component={AdminLogin} />
+       </Routes>
+      </BrowserRouter>
     </div>
   );
 }
